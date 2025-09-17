7ea1b40c348bd78ac51597d1bbb376b91fce09b0
FAANG_CODES <100085818+naveen-here@users.noreply.github.com>
2025-09-18 01:16:22 +0530

    Add solution for Merge Sorted Array

--- LeetCode/Merge-Sorted-Array/Solution.cpp
+++ LeetCode/Merge-Sorted-Array/Solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int j = 0 ,i = m; j<n; j++){
+        for (int j = 0, i = m; j<n; j++){
             nums1[i] = nums2[j];
             i++;
         }
