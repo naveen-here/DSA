e27bfd5437e876ce12aca0113307ca90222d1d99
FAANG_CODES <100085818+naveen-here@users.noreply.github.com>
2025-12-15 02:23:17 +0530

    Add solution for Remove Duplicate Letters

--- LeetCode/Remove-Duplicate-Letters/Solution.py
+++ LeetCode/Remove-Duplicate-Letters/Solution.py
@@ -1,17 +1,17 @@
-class Solution:
-    def removeDuplicateLetters(self, s: str) -> str:
-        last_occr = {}
-        stack = []
-        visited = set ()
-
-        for i in range (len(s)):
-            last_occr[s[i]] = i
-        for i in range (len(s)):
-            if s[i] not in visited :
-                while(stack and stack[-1] > s[i] and last_occr[stack[-1]] > i):
-                    visited.remove(stack.pop())
-                
-                stack.append(s[i])
-                visited.add(s[i])
-        return "" .join(stack)
-        
+1class Solution:
+2    def removeDuplicateLetters(self, s: str) -> str:
+3        last_occr = {}
+4        stack = []
+5        visited = set ()
+6
+7        for i in range (len(s)):
+8            last_occr[s[i]] = i
+9        for i in range (len(s)):
+10            if s[i] not in visited :
+11                while(stack and stack[-1] > s[i] and last_occr[stack[-1]] > i):
+12                    visited.remove(stack.pop())
+13                
+14                stack.append(s[i])
+15                visited.add(s[i])
+16        return "" .join(stack)
+17        
